c76a56296e330820f16c1767d479df1a5623c4cd
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-08-10 18:36:56 +0500

    0

--- attendance-management-frontend/src/redux/actions/types.jsx
+++
@@ -1,7 +0,0 @@
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_FAIL = 'REGISTER_FAIL';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAIL = 'LOGIN_FAIL';
-export const LOGOUT = 'LOGOUT';
-export const GET_TIMESLOTS = 'GET_TIMESLOTS';
-export const MARK_ATTENDANCE = 'MARK_ATTENDANCE';
